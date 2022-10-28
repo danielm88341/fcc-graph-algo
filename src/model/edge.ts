fbf9d6ad514e5d671789fa773439d00a9048b148
Daniel Mills <danielm88341@gmail.com>
2022-10-27 22:03:13 -0400

    edge and vertex

--- src/model/edge.ts
+++ src/model/edge.ts
@@ -1,3 +1,16 @@
+import Vertex from "./vertex";
+
 export default class Edge {
+
+    weight: number | null;
+    start: Vertex;
+    end: Vertex;
+
+    constructor(start: Vertex, end: Vertex, weight: number | null) {
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
+
 
 }
--- src/model/vertex.ts
+++ src/model/vertex.ts
@@ -8,4 +8,28 @@ export default class Vertex {
         this.data = inputData;
         this.edges = [];
     }
+
+    addEdge(endVertex: Vertex, weight?: number): number {
+        return this.edges.push(new Edge(this, endVertex, weight));
+    }
+    removeEdge(endVertex: Vertex): number {
+        this.edges = this.edges.filter(value => {return value.end !== endVertex});
+        return this.edges.length;
+    }
+
+    print(showWeight: boolean) {
+        if(this.edges.length === 0) {
+            console.log(this.data + '--> Empty');
+            return;
+        }
+
+        let message = '';
+        this.edges.forEach(value => {
+            message += value.start.data + ' --> ' + value.end.data;
+            if(showWeight) {
+                message += ` ( ${value.weight})'`;
+            }
+            message += ', ';
+        });
+    }
 }
